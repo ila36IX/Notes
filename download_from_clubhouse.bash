707b3dcaa4629e3cbadc812044cd47d40d85fea4
ila36IX <jbariali002@gmail.com>
2024-01-29 01:23:55 +0100

    Update

---
+++ download_from_clubhouse.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#################################################################
+########################## HOW TO USE ###########################
+#################################################################
+# Your file that consists of chuncks linkes should be named input
+# and put in the somae directory as this script
+#################################################################
+
+function trap_ctrlc ()
+{
+	# perform cleanup here
+	echo "You've canceled the scirpt"
+	echo "Surly you have you're reasons ✌️"
+	rm .*.ts
+	exit 2
+}
+
+trap "trap_ctrlc" 2
+
+python3 .download_from_clubhouse.py 2>/dev/null
+
+ls -1v .*.ts | while read line
+do
+	cat $line >> output.ts
+done
+
+rm .*.ts
+
+echo "Your file is under the name input.ts, have fun!"
+echo "\nDone ✅"
